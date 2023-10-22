8458d6fec916599f422ff7051b09a0b98a4a9ad9
nbrittain3 <nbrittain3@gatech.edu>
2023-10-22 16:05:36 -0400

    minor

--- lua/handdara/remap.lua
+++ lua/handdara/remap.lua
@@ -1,2 +1,10 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc = "Handdara: `:Ex`"})
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, {desc = "Handdara: Open undo tree"})
+
+-- Better feeling close
+
+-- Access system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+
