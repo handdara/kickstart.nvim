0233e76af24f615bb34425bcabe74c6568b66d6a
nbrittain3 <nbrittain3@gatech.edu>
2023-10-23 23:27:02 -0400

    move around windows with leader, alt close

--- lua/handdara/remap.lua
+++ lua/handdara/remap.lua
@@ -1,11 +1,23 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc = "Handdara: `:Ex`"})
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, {desc = "Handdara: Open undo tree"})
+local wk = require("which-key")
 
--- Better feeling close
-vim.keymap.set('i', "<C-c>", "<Esc>")
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Handdara: `:Ex`" })
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = "Handdara: Open undo tree" })
+
+-- Better feeling exit insert mode
+vim.keymap.set('i', "<M-c>", "<Esc>")
+
+-- Moving around windows
+vim.keymap.set('n', '<leader>tj', '<C-w>j', { desc = 'Move [T]o window down' })
+vim.keymap.set('n', '<leader>tk', '<C-w>k', { desc = 'Move [T]o window up' })
+vim.keymap.set('n', '<leader>th', '<C-w>h', { desc = 'Move [T]o window left' })
+vim.keymap.set('n', '<leader>tl', '<C-w>l', { desc = 'Move [T]o window right' })
+wk.register(
+	{ t = { name = "+[T]o window", } },
+	{ prefix = "<leader>" }
+)
 
 -- Access system clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- Keep in center after move
